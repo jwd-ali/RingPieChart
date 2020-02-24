4f4adf39c5f6425a1fd583922a217fecd4ca861c
jawad <jawad@digitify.com>
2020-02-24 17:40:12 +0500

    add more animations in view

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -8,27 +8,39 @@
 
 import UIKit
 import RingPieChart
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        
-        
-        // percentages array with colors Array iniitializer of RingPieChart
-        //you can remove animate property if you want to animate piechart.
-        //Animate property is true by default
-        
-        let cir = Circular(percentages: [42,27,18,13], colors: [#colorLiteral(red: 0.2768642902, green: 0.552749753, blue: 0.9582422376, alpha: 1),#colorLiteral(red: 0.6800097227, green: 0.2969456911, blue: 0.8304955363, alpha: 1),#colorLiteral(red: 0.9592140317, green: 0.4973406196, blue: 0.08628573269, alpha: 1),#colorLiteral(red: 0.9573473334, green: 0.2803060114, blue: 0.4532059431, alpha: 1)],animated: true)
-        cir.frame = CGRect(x: 50, y: 50, width: 250, height: 250)
-        cir.center = self.view.center
-   //     cir.lineWidth = 12 you can set the chart width with this lineWidth property
-        self.view .addSubview(cir)
-        
-     
-        
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak private var percentageSegment: UISegmentedControl!
+    @IBOutlet weak private var animationSegment: UISegmentedControl!
+    private var cir :Circular!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        addToView()
     }
-
-
+    
+    private func addToView() {
+        if (cir != nil)  {
+            cir.removeFromSuperview()
+        }
+        
+        cir = Circular(percentages: [42,27,18,13], colors: [#colorLiteral(red: 0.2768642902, green: 0.552749753, blue: 0.9582422376, alpha: 1),#colorLiteral(red: 0.6800097227, green: 0.2969456911, blue: 0.8304955363, alpha: 1),#colorLiteral(red: 0.9592140317, green: 0.4973406196, blue: 0.08628573269, alpha: 1),#colorLiteral(red: 0.9573473334, green: 0.2803060114, blue: 0.4532059431, alpha: 1)])
+        cir.animationType = AnimationStyle.init(rawValue: animationSegment.selectedSegmentIndex) ?? .animationFan
+        cir.showPercentageStyle = PercentageStyle.init(rawValue: percentageSegment.selectedSegmentIndex) ?? .none
+        cir.frame = CGRect(x: 50, y: 50, width: 250, height: 250)
+        cir.lineWidth = 10
+        cir.center = self.view.center
+        
+        self.view.addSubview(cir)
+    }
+    
+    @IBAction private func animationStyles(_ sender: UISegmentedControl) {
+        addToView()
+    }
+    
+    @IBAction private func percentagestyles(_ sender: UISegmentedControl) {
+        addToView()
+    }
+    
 }
 
--- Sources/RingPieChart/Circular.swift
+++ Sources/RingPieChart/Circular.swift
@@ -7,22 +7,45 @@
 //
 
 import UIKit
+// MARK: - Enums
+public enum AnimationStyle: Int {
+    case animationFanAll
+    case animationFan
+    case animationFadeIn
+    case animationthreeD
+    case none
+}
 
-public class Circular: UIView {
-
- //   MARK:- Private Variable
-    private let fillLayer = CAShapeLayer()
+public enum PercentageStyle : Int {
+    case none
+    case inward
+    case outward
+    case over
     
-    private var _percentages: [Double]
+}
+open class Circular: UIView {
     
-    private var _colors: [UIColor]
+    // MARK: - Public Properties
+    public var animationType: AnimationStyle {
+        get {
+            return _animationType
+        }
+        set(newValue) {
+            _animationType = newValue
+            setNeedsDisplay()
+        }
+    }
     
-    private var isAnimationOn = true
+    public var showPercentageStyle: PercentageStyle {
+        get {
+            return _showPercentageStyle
+        }
+        set(newValue) {
+            _showPercentageStyle = newValue
+            setNeedsDisplay()
+        }
+    }
     
-    private var _lineWidth = CGFloat( 10.0)
-    
-    
-    //   MARK:- Public Variable
     public var lineWidth: CGFloat {
         get {
             return _lineWidth
@@ -33,10 +56,23 @@ public class Circular: UIView {
         }
     }
     
+    //  MARK:- Private Variable
+    
+    private var _percentages: [Double]
+    
+    private var _colors: [UIColor]
+    
+    private var _lineWidth = CGFloat( 10.0)
+    
+    private var _animationType: AnimationStyle
+    
+    private var _showPercentageStyle: PercentageStyle
+    
+    
+    
     //MARK:- draw
     
     override public func draw(_ rect: CGRect) {
-        
         var startAngle = -90.0
         
         for i in 0..<_percentages.count {
@@ -44,37 +80,56 @@ public class Circular: UIView {
             let endAngle = startAngle + ( _percentages[i] * 3.6 ) - 4
             
             let shapeLayer =   self.addArac(with: _colors[i], in: rect, startAngle: startAngle, endAngle: endAngle)
-            startAngle = (endAngle + 4 )
+            showAnimationStyle(index: Double(i), shapeLayer: shapeLayer, startAngle: startAngle, endAngle: endAngle)
+            showPercentages(midAngel:startAngle + (endAngle - startAngle)/2, percentage: _percentages[i])
             
-            if isAnimationOn {
-                oppacityAnimation(index: Double(i), shape: shapeLayer)
-            }
+            startAngle = (endAngle + 4 )
         }
         
     }
     
-    
     //MARK:- inializer
     
-   public init(percentages:[Double],colors:[UIColor],animated:Bool = false) {
+    public init(percentages:[Double],colors:[UIColor],aimationType:AnimationStyle = .animationFanAll , showPercentageStyle: PercentageStyle = .none) {
         
         self._percentages = percentages
         self._colors = colors
-        self.isAnimationOn = animated
+        self._animationType = aimationType
+        self._showPercentageStyle = showPercentageStyle
         super.init(frame:CGRect.zero)
         self.backgroundColor = .clear
+        self.clipsToBounds = false
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         // super.init(coder: coder)
         fatalError("init(coder:) has not been implemented")
     }
     
     
     //MARK:- Animations Functions
+    private func showAnimationStyle(index:Double,shapeLayer:CAShapeLayer,startAngle:Double,endAngle:Double) {
+        
+        switch _animationType {
+        case .animationFanAll:
+            maskEachLayerAnimation(startAngal: startAngle, endAngal: endAngle + 4 , shape: shapeLayer)
+        case .animationFan:
+            if Int(index) == _percentages.count - 1 {
+                maskAnimation()
+            }
+        case .animationFadeIn:
+            oppacityAnimation(index: index, shape: shapeLayer)
+        case .animationthreeD:
+            transformAnimation(index: index, shape: shapeLayer)
+        case .none:
+            break
+            
+        }
+    }
+    
     private  func oppacityAnimation(index:Double,shape:CAShapeLayer) {
         shape.opacity = 0
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5 + Double(index)/2.5 ) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + Double(index)/2.5 ) {
             shape.opacity = 1
             let animation = CABasicAnimation(keyPath: "opacity")
             animation.fromValue = 0
@@ -86,23 +141,124 @@ public class Circular: UIView {
     
     private func transformAnimation(index:Double,shape:CAShapeLayer){
         shape.opacity = 0
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5 + Double(index)/2.5 ) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + Double(index)/2.5 ) {
             shape.opacity = 1
             let animation = CABasicAnimation(keyPath: "transform")
             animation.fromValue = CATransform3DMakeScale(0, 0, 1)
             animation.toValue = CATransform3DIdentity
-            animation.duration = 0.4
+            animation.duration = 1
             shape.add(animation, forKey: nil)
+        }
+    }
+    
+    private func maskEachLayerAnimation(startAngal:Double,endAngal:Double,shape:CAShapeLayer){
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.green.cgColor
+        shapeLayer.lineWidth = max( bounds.maxX,bounds.maxY)/5
+        shapeLayer.frame = bounds
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        let longestSide = max(bounds.height,bounds.width)
+        shapeLayer.path = UIBezierPath(arcCenter: center, radius: longestSide/2, startAngle: CGFloat(startAngal).deg2rad(), endAngle: CGFloat(endAngal ).deg2rad(), clockwise: true).cgPath
+        shapeLayer.strokeEnd = 0
+        shape.mask = shapeLayer
+        
+        addAnimationToLayer(toLayer: shape, fromLayer: shapeLayer)
+        
+    }
+    
+    private func maskAnimation() {
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.white.cgColor
+        shapeLayer.lineWidth = max( bounds.maxX,bounds.maxY)/2
+        shapeLayer.frame = bounds
+        let path = UIBezierPath(arcCenter: CGPoint(x:bounds.midX,y:bounds.midY), radius:max( bounds.maxX/2,bounds.maxY/2), startAngle: CGFloat(-89.0).deg2rad(), endAngle: CGFloat( 270.0).deg2rad(), clockwise: true)
+        
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeEnd = 0
+        self.layer.mask = shapeLayer
+        
+        addAnimationToLayer(toLayer: self.layer, fromLayer: shapeLayer)
+        
+    }
+
+    private func addAnimationToLayer(toLayer:CALayer , fromLayer:CAShapeLayer) {
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            toLayer.mask = nil
+        }
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.beginTime = CACurrentMediaTime() + 0.3
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.duration = 1
+        fromLayer.add(animation, forKey: "line")
+        
+        CATransaction.commit()
+        
+    }
+    
+    //MARK:- show percentages
+    private func showPercentages(midAngel:Double, percentage:Double) {
+        
+        guard let radius = getRadiusOfPercentage() else {
+            return
+        }
+        
+        let center = CGPoint(x: bounds.maxX / 2, y: bounds.maxY / 2)
+        
+        let  x = center.x  + (radius) * CGFloat(cos(CGFloat(midAngel).deg2rad()))
+        let  y = center.y  + (radius) * CGFloat(sin(CGFloat(midAngel).deg2rad()))
+        
+        
+        let percentageLabel  = UILabel(frame: CGRect.zero)
+        percentageLabel.frame = CGRect.zero
+        percentageLabel.text = String(percentage)
+        percentageLabel.textColor = .black
+        percentageLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        percentageLabel.sizeToFit()
+        percentageLabel.center = CGPoint(x:x,y:y)
+        addSubview(percentageLabel)
+        percentageLabel.alpha = 0
+        
+        var delay = 1.5
+        if self.animationType == .none {
+            delay = 0
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: delay, options: .curveEaseOut, animations: {
+            percentageLabel.alpha = 1
+        })
+    }
+    
+    private func getRadiusOfPercentage() -> CGFloat? {
+        let longestSide = max(bounds.height,bounds.width)
+        switch self.showPercentageStyle {
+        case .inward:
+            return longestSide/3 - lineWidth
+        case .over:
+            return longestSide/2 - lineWidth
+        case .outward:
+            return longestSide/2  + lineWidth + 5
+        case .none:
+            return nil
         }
     }
     
     //MARK:- Drawing Code
     
-   private func addArac(with color:UIColor ,in rect:CGRect, startAngle:Double , endAngle:Double)-> CAShapeLayer {
+    private func addArac(with color:UIColor ,in rect:CGRect, startAngle:Double , endAngle:Double)-> CAShapeLayer {
         
         let center = CGPoint(x: rect.maxX / 2, y: rect.maxY / 2)
         
-        let longestSide = rect.height < rect.width ? rect.height : rect.width
+        let longestSide = max(rect.height,rect.width)
         
         let lineWidth = CGFloat(self._lineWidth / 20)
         
@@ -131,8 +287,6 @@ public class Circular: UIView {
         
         path.move(to: CGPoint(x:x4,y:y4))
         path.addQuadCurve(to: CGPoint(x:x3,y:y3), controlPoint: CGPoint(x:x5,y:y5))
-        
-        
         
         path.addArc(withCenter:center, radius:outerRadious, startAngle: startAngle.deg2rad()  , endAngle: endAngle.deg2rad(), clockwise: true)
         
